e7c4e0c5fd28cda6d731342ac1ff36f4ddf45270
James A. Stewart <jastew09@Jamess-MacBook-Pro.local>
2022-07-05 21:10:47 -0600

    Moving .h info to .c

--- src/ctof_io.c
+++ src/ctof_io.c
@@ -1,10 +1,10 @@
 /* ---------------------------------------------------------------------------------------------------------------------------------
-   Socorro is a plane-wave density functional theory code for solid-state electronic structure calculations.                        
-   See the README file in the top-level directory.                                                                                  
-   
-   Copyright 2011 National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under the terms of contract DE-NA0003525      
-   with NTESS, the United States Government retains certain rights to this software. This software is distributed uner the          
-   modified Berkeley Software Distribution (BSD) License.                                                                           
+   Socorro is a plane-wave density functional theory code for solid-state electronic structure calculations.
+   See the README file in the top-level directory.
+
+   Copyright 2011 National Technology and Engineering Solutions of Sandia, LLC (NTESS).
+   This software is distributed uner the modified Berkeley Software Distribution (BSD) License.
+   Under the terms of contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights to this software.
 --------------------------------------------------------------------------------------------------------------------------------- */
 
 /* Reflect C low-level binary I/O functions to Fortran */
@@ -13,7 +13,10 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#include "ctof_io.h"
+/* ------------------------------------------------------------------ */
+
+typedef int longint;                                /* 4 byte integer */
+typedef long longlongint;                           /* 8 byte integer */
 
 /* ------------------------------------------------------------------ */
 
